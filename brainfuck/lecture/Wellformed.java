7ae36c8c5cc8428c25abcd3859f625c84ecdcbbd
Lehmann Florian <florian576@gmail.com>
2016-11-05 16:30:32 +0100

    Revert "-fix"
    This reverts commit 2109bc445e131554e0e80fbd47e0685403473480.
    
    fff

--- brainfuck/lecture/Wellformed.java
+++ brainfuck/lecture/Wellformed.java
@@ -35,10 +35,14 @@ public class Wellformed extends Lecture{
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     static public popStack()
 =======
     public void popStack()
 >>>>>>> eb50e9a5813f9d8b1c15647038e478fb6688c825
+=======
+    public popStack()
+>>>>>>> parent of 2109bc4... -fix
 =======
     public popStack()
 >>>>>>> parent of 2109bc4... -fix
@@ -59,10 +63,14 @@ public class Wellformed extends Lecture{
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     static public pushStack(String line)
 =======
     public void pushStack(String line)
 >>>>>>> eb50e9a5813f9d8b1c15647038e478fb6688c825
+=======
+    public pushStack(String line)
+>>>>>>> parent of 2109bc4... -fix
 =======
     public pushStack(String line)
 >>>>>>> parent of 2109bc4... -fix
