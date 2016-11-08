014385474e3f1a72ba185f0544b9e5039fbe84b8
Shanoi <olivboulet@laposte.net>
2016-11-08 19:25:30 +0100

    Wellformed Image Ajout

--- brainfuck/scommands/LongSyntaxe.java
+++ brainfuck/scommands/LongSyntaxe.java
@@ -3,32 +3,33 @@ package brainfuck.scommands;
 import brainfuck.command.EnumCommands;
 import brainfuck.lecture.Wellformed;
 
-public class LongSyntaxe implements SyntaxeCommand
-{
+public class LongSyntaxe implements SyntaxeCommand {
+
     @Override
-    public void verify(String line)
-    {
-	if(!EnumCommands.isCommand(line))
-	    System.exit(4);
-	if(line.equals("JUMP"))
-	    Wellformed.pushStack("[");
-    if(line.equals("BACK") && Wellformed.emptyStack())
-        System.exit(4);
-    if(line.equals("BACK") && !Wellformed.emptyStack())
-	    Wellformed.popStack();
-	
+    public void verify(String line) {
+        if (!EnumCommands.isCommand(line)) {
+            System.exit(4);
+        }
+        if (line.equals("JUMP")) {
+            Wellformed.pushStack("[");
+        }
+        if (line.equals("BACK") && Wellformed.emptyStack()) {
+            System.exit(4);
+        }
+        if (line.equals("BACK") && !Wellformed.emptyStack()) {
+            Wellformed.popStack();
+        }
+
     }
 
     @Override
-    public void translate(String line)
-    {
-	System.out.print(EnumCommands.toCommand(line).getShort());
+    public void translate(String line) {
+        System.out.print(EnumCommands.toCommand(line).getShort());
     }
 
     @Override
-    public void run(String line)
-    {
-	EnumCommands.toCommand(line).getCommand().execute();
+    public void run(String line) {
+        EnumCommands.toCommand(line).getCommand().execute();
     }
 
 }
--- brainfuck/scommands/ShortSyntaxe.java
+++ brainfuck/scommands/ShortSyntaxe.java
@@ -3,36 +3,35 @@ package brainfuck.scommands;
 import brainfuck.command.EnumCommands;
 import brainfuck.lecture.Wellformed;
 
-public class ShortSyntaxe implements SyntaxeCommand
-{
+public class ShortSyntaxe implements SyntaxeCommand {
+
     @Override
-    public void verify(String line)
-    {
-	for( int i = 0; i < line.length(); i++ )
-	{
-	    if(!EnumCommands.isCommand(Character.toString(line.charAt(i))))
-		System.exit(4);
-	    if(Character.toString(line.charAt(i)).equals("["))
-		Wellformed.pushStack("[");
-        if(Character.toString(line.charAt(i)).equals("]") && Wellformed.emptyStack())
-            System.exit(4);
-        if(Character.toString(line.charAt(i)).equals("]") && !Wellformed.emptyStack())
-		Wellformed.popStack();
-	    
-	    
-	}
+    public void verify(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            if (!EnumCommands.isCommand(Character.toString(line.charAt(i)))) {
+                System.exit(4);
+            }
+            if (Character.toString(line.charAt(i)).equals("[")) {
+                Wellformed.pushStack("[");
+            }
+            if (Character.toString(line.charAt(i)).equals("]") && Wellformed.emptyStack()) {
+                System.exit(4);
+            }
+            if (Character.toString(line.charAt(i)).equals("]") && !Wellformed.emptyStack()) {
+                Wellformed.popStack();
+            }
+
+        }
     }
 
     @Override
-    public void translate(String line)
-    {
-	System.out.print(line);
+    public void translate(String line) {
+        System.out.print(line);
     }
 
     @Override
-    public void run(String line)
-    {
-	EnumCommands.toCommand(line).getCommand().execute();
+    public void run(String line) {
+        EnumCommands.toCommand(line).getCommand().execute();
     }
 
 }
