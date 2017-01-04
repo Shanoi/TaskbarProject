d9801539fa9138707a9de12656def1ae87e4f35d
Kymine <kymine59@gmail.com>
2017-01-04 17:23:28 +0100

    seb: Ajout Test Run + fichier de test

--- src/test/java/brainfuck/lecture/RunIT.java
+++ src/test/java/brainfuck/lecture/RunIT.java
@@ -1,11 +1,8 @@
 package brainfuck.lecture;
 
 import brainfuck.memory.ComputationalModel;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import java.io.File;
 
@@ -35,14 +32,22 @@ public class RunIT {
     @After
     public void tearDown() {
     }
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
 
 
     @Test
     public void testRead() throws Exception{
         String path=new File("").getAbsolutePath();
-        System.out.println(path);
         Run run=new Run(path+"/src/test/java/brainfuck/lecture/FichierTestUnit.txt");
         run.load();
         assertEquals(run.getFichier().list.size(),1);
+    }
+    @Test
+    public void testReadError() throws Exception{
+        String path=new File("").getAbsolutePath();
+        Run run=new Run(path+"/src/test/java/brainfuck/lecture/FichierTestUnit2.txt");
+        exit.expectSystemExit();
+        run.load();
     }
 }
