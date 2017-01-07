457ea63e7d7aba26911948b4587a2731e02b779b
LehmannF <florian.lehmann@etu.unice.fr>
2017-01-07 21:17:46 +0100

    fichiers de tests

---
+++ src/test/java/brainfuck/command/LeftTest.java
@@ -0,0 +1,48 @@
+package brainfuck.command;
+
+import brainfuck.memory.ComputationalModel;
+import org.junit.*;
+
+import static org.junit.Assert.*;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+
+/**
+ *
+ * @author TeamTaskbar
+ */
+public class LeftTest {
+    
+    public LeftTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
+    /**
+     * Test of execute method, of class Left.
+     */
+    @Test
+    public void testExecute() {
+        ComputationalModel cm=new ComputationalModel();
+        cm.init();
+        Command command=new Left();
+        exit.expectSystemExit();
+        command.execute();
+    }
+    
+}
