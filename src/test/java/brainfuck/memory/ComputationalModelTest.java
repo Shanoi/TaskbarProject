593ad30fff8f4bb0293ee9000c5a8b11e9cc1ff6
Kymine <kymine59@gmail.com>
2017-01-07 23:21:40 +0100

    seb: ajout test computationalmodel

--- src/test/java/brainfuck/memory/ComputationalModelTest.java
+++ src/test/java/brainfuck/memory/ComputationalModelTest.java
@@ -32,136 +32,26 @@ public class ComputationalModelTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of init method, of class ComputationalModel.
-     */
     @Test
-    public void testInit() {
-        System.out.println("init");
-        ComputationalModel instance = new ComputationalModel();
-        instance.init();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void initTest(){
+        ComputationalModel cm=new ComputationalModel();
+        cm.init();
+        assertEquals(cm.getCurrentCaseValue(),0);
     }
 
-    /**
-     * Test of getMemorySize method, of class ComputationalModel.
-     */
     @Test
-    public void testGetMemorySize() {
-        System.out.println("getMemorySize");
-        ComputationalModel instance = new ComputationalModel();
-        int expResult = 0;
-        int result = instance.getMemorySize();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void setCurrentCaseValueTest(){
+        ComputationalModel cm=new ComputationalModel();
+        cm.init();
+        cm.setCurrentCaseValue((byte)100);
+        assertEquals(cm.getCurrentCaseValue(),100);
+    }
+    @Test
+    public void setCurrentIndiceTest(){
+        ComputationalModel cm=new ComputationalModel();
+        cm.init();
+        cm.setCurrentIndice(4);
+        assertEquals(cm.getCurrentIndice(),4);
     }
 
-    /**
-     * Test of getCurrentCaseValue method, of class ComputationalModel.
-     */
-    @Test
-    public void testGetCurrentCaseValue() {
-        System.out.println("getCurrentCaseValue");
-        ComputationalModel instance = new ComputationalModel();
-        int expResult = 0;
-        int result = instance.getCurrentCaseValue();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCurrentCaseValue method, of class ComputationalModel.
-     */
-    @Test
-    public void testSetCurrentCaseValue() {
-        System.out.println("setCurrentCaseValue");
-        byte n = 0;
-        ComputationalModel instance = new ComputationalModel();
-        instance.setCurrentCaseValue(n);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCurrentIndice method, of class ComputationalModel.
-     */
-    @Test
-    public void testGetCurrentIndice() {
-        System.out.println("getCurrentIndice");
-        ComputationalModel instance = new ComputationalModel();
-        int expResult = 0;
-        int result = instance.getCurrentIndice();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCurrentIndice method, of class ComputationalModel.
-     */
-    @Test
-    public void testSetCurrentIndice() {
-        System.out.println("setCurrentIndice");
-        int p = 0;
-        ComputationalModel instance = new ComputationalModel();
-        instance.setCurrentIndice(p);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getI method, of class ComputationalModel.
-     */
-    @Test
-    public void testGetI() {
-        System.out.println("getI");
-        ComputationalModel instance = new ComputationalModel();
-        int expResult = 0;
-        int result = instance.getI();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setI method, of class ComputationalModel.
-     */
-    @Test
-    public void testSetI() {
-        System.out.println("setI");
-        int d = 0;
-        ComputationalModel instance = new ComputationalModel();
-        instance.setI(d);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of affichememoire method, of class ComputationalModel.
-     */
-    @Test
-    public void testAffichememoire() {
-        System.out.println("affichememoire");
-        ComputationalModel.affichememoire();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of toString method, of class ComputationalModel.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        ComputationalModel instance = new ComputationalModel();
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
 }
