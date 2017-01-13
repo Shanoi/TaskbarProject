ff8af0977292912768a696557fe7991679f2b1a4
Shanoi <olivboulet@laposte.net>
2017-01-13 20:55:40 +0100

    Ajout de TCpp dans le launcher

--- src/main/java/brainfuck/memory/Launcher.java
+++ src/main/java/brainfuck/memory/Launcher.java
@@ -106,6 +106,16 @@ public class Launcher {
             }
 
         }
+        
+        for (int i = 0; i < args.length; i++) {
+            
+            if (args[i].equals("--TCpp")) {
+
+                new TradCpp(path).execute();
+            }
+
+            
+        }
 
         run.execute();
         ComputationalModel.affichememoire();
